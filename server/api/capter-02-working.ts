2e2ad70ded6ef1e69052f7dde0f3ce0587910384
김주찬 <rlawncks125@naver.com>
2023-07-24 08:42:55 +0900

    실무형 json import 에러;

--- server/api/capter-02-working.ts
+++ server/api/capter-02-working.ts
@@ -1,5 +1,5 @@
 //@ts-ignore
-import data from "../datas/네트워크보안-실무형.json" assert { type: "json" };
+// import data from "../datas/네트워크보안-실무형.json" assert { type: "json" };
 
 // import { PREPARING_QUESTION_PATH } from "../../common/path";
 
