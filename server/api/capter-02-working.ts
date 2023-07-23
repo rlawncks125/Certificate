792c3400e7457cf60a497db424d5c9b02ff49a09
김주찬 <rlawncks125@naver.com>
2023-07-24 08:39:08 +0900

    네트워크 실무형

--- server/api/capter-02-working.ts
+++ server/api/capter-02-working.ts
@@ -4,7 +4,9 @@
 import { PREPARING_QUESTION_PATH } from "../../common/path";
 
 export default defineEventHandler(async () => {
-  const res = await fetch(PREPARING_QUESTION_PATH)
+  const res = await fetch(
+    "https://res.cloudinary.com/dhdq4v4ar/raw/upload/v1690155600/%EC%A0%95%EB%B3%B4%EB%B3%B4%EC%95%88/%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC%EB%B3%B4%EC%95%88-%EC%8B%A4%EB%AC%B4%ED%98%95_ubccfw.json"
+  )
     .then((res) => res.json())
     .catch((err) => {});
 
