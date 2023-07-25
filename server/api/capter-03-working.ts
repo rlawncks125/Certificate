a780a91371a8989b31f8e25dfa75176424d93e39
김주찬 <rlawncks125@naver.com>
2023-07-25 15:08:16 +0900

    침해사고 분석 및 대응 단답형

--- server/api/capter-03-working.ts
+++ server/api/capter-03-working.ts
@@ -1,6 +1,8 @@
+// 어플리케이션-실무형
+
 export default defineEventHandler(async () => {
   const res = await fetch(
-    "https://res.cloudinary.com/dhdq4v4ar/raw/upload/v1690254814/%EC%A0%95%EB%B3%B4%EB%B3%B4%EC%95%88/%EC%96%B4%ED%94%8C%EB%A6%AC%EC%BC%80%EC%9D%B4%EC%85%98-%EC%8B%A4%EB%AC%B4%ED%98%95_d9it8k.json"
+    "https://res.cloudinary.com/dhdq4v4ar/raw/upload/v1690255741/%EC%A0%95%EB%B3%B4%EB%B3%B4%EC%95%88/%EC%96%B4%ED%94%8C%EB%A6%AC%EC%BC%80%EC%9D%B4%EC%85%98-%EC%8B%A4%EB%AC%B4%ED%98%95_bynivg.json"
   )
     .then((res) => res.json())
     .catch((err) => {});
