42c637c33187bf54488cd26b3a0c21d31b1bee77
김주찬 <rlawncks125@naver.com>
2023-07-26 01:13:00 +0900

    ..

--- server/api/capter-04-desc.ts
+++ server/api/capter-04-desc.ts
@@ -1,14 +1,14 @@
 // 침해사고분석및대응-서술형
 //@ts-ignore
-import data from "../datas/침해사고분석및대응-서술형.json" assert { type: "json" };
+// import data from "../datas/침해사고분석및대응-서술형.json" assert { type: "json" };
 
 import { PREPARING_QUESTION_PATH } from "../../common/path";
 
 export default defineEventHandler(async () => {
-  return data;
-  // const res = await fetch(PREPARING_QUESTION_PATH)
-  //   .then((res) => res.json())
-  //   .catch((err) => {});
+  // return data;
+  const res = await fetch(PREPARING_QUESTION_PATH)
+    .then((res) => res.json())
+    .catch((err) => {});
 
   // return res;
 });
